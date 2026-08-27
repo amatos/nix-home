fe4d43ffe3f29ca2a3afe07aa7cc39d9bf276a2c
Alberth Matos <alberth@matos.cc>
2026-08-26 23:48:52 -0400

    feat: updated fonts
    Signed-off-by: Alberth Matos <alberth@matos.cc>

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,15 +1,27 @@
 { pkgs, ... }: {
   fonts.fontconfig.enable = true;
 
-  home.packages = with pkgs.ioskeley-mono; [
-    normal-NF
-    normal-NL-NF
-    normal
-    condensed
-    normal-term
-    condensed-NF
-    semiCondensed
-    normal-term-NF
-    condensed-term
+  home.packages = with pkgs; [
+    ioskeley-mono.normal-NF
+    ioskeley-mono.normal-NL-NF
+    ioskeley-mono.normal
+    ioskeley-mono.condensed
+    ioskeley-mono.normal-term
+    ioskeley-mono.condensed-NF
+    ioskeley-mono.semiCondensed
+    ioskeley-mono.normal-term-NF
+    ioskeley-mono.condensed-term
+    anonymousPro
+    font-awesome
+    hack-font
+    inconsolata
+    jetbrains-mono
+    liberation_ttf
+    nerd-fonts.dejavu-sans-mono
+    nerd-fonts.hack
+    nerd-fonts.inconsolata-go
+    nerd-fonts.jetbrains-mono
   ];
+
+
 }
