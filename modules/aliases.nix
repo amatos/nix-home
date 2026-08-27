c2c3428924e2f95c7faaf3081845f1818499a573
Alberth Matos <alberth@matos.cc>
2026-08-26 22:58:40 -0400

    feat: added shell aliases for nix/home-manager
    Signed-off-by: Alberth Matos <alberth@matos.cc>

--- modules/aliases.nix
+++ modules/aliases.nix
@@ -8,6 +8,33 @@
       esac
       popd
     }
+
+    home-switch() {
+      pushd "$HOME/Projects/nix-home" || return
+      case "$(uname -s)" in
+        Darwin) home-manager switch --flake .#darwin ;;
+        Linux) home-manager switch --flake .#nixos ;;
+      esac
+      popd
+    }
+
+    nix-build() {
+      pushd "$HOME/Projects/nix-dendritic" || return
+      case "$(uname -s)" in
+        Darwin) sudo darwin-rebuild build --flake .#$(hostname) ;;
+        Linux) sudo nixos-rebuild build --flake .#$(hostname) ;;
+      esac
+      popd
+    }
+
+    nix-switch() {
+      pushd "$HOME/Projects/nix-dendritic" || return
+      case "$(uname -s)" in
+        Darwin) sudo darwin-rebuild switch --flake .#$(hostname) ;;
+        Linux) sudo nixos-rebuild switch --flake .#$(hostname) ;;
+      esac
+      popd
+    }
   '';
 
   programs.zsh.initContent = ''
@@ -16,6 +43,33 @@
       case "$(uname -s)" in
         Darwin) home-manager build --flake .#darwin ;;
         Linux) home-manager build --flake .#nixos ;;
+      esac
+      popd
+    }
+
+    home-switch() {
+      pushd "$HOME/Projects/nix-home" || return
+      case "$(uname -s)" in
+        Darwin) home-manager switch --flake .#darwin ;;
+        Linux) home-manager switch --flake .#nixos ;;
+      esac
+      popd
+    }
+
+    nix-build() {
+      pushd "$HOME/Projects/nix-dendritic" || return
+      case "$(uname -s)" in
+        Darwin) sudo darwin-rebuild build --flake .#$(hostname) ;;
+        Linux) sudo nixos-rebuild build --flake .#$(hostname) ;;
+      esac
+      popd
+    }
+
+    nix-switch() {
+      pushd "$HOME/Projects/nix-dendritic" || return
+      case "$(uname -s)" in
+        Darwin) sudo darwin-rebuild switch --flake .#$(hostname) ;;
+        Linux) sudo nixos-rebuild switch --flake .#$(hostname) ;;
       esac
       popd
     }
@@ -31,4 +85,38 @@
     end
     popd
   '';
+
+  programs.fish.functions.home-switch = ''
+    pushd $HOME/Projects/nix-home
+    switch (uname -s)
+      case Darwin
+        home-manager switch --flake .#darwin
+      case Linux
+        home-manager switch --flake .#nixos
+    end
+    popd
+  '';
+
+  programs.fish.functions.nix-build = ''
+    pushd $HOME/Projects/nix-dendritic
+    switch (uname -s)
+      case Darwin
+        darwin-rebuild build --flake .#$(hostname)
+      case Linux
+        nixos-rebuild build --flake .#$(hostname)
+    end
+    popd
+  '';
+
+  programs.fish.functions.nix-switch = ''
+    pushd $HOME/Projects/nix-dendritic
+    switch (uname -s)
+      case Darwin
+        sudo darwin-rebuild switch --flake .#$(hostname)
+      case Linux
+        sudo nixos-rebuild switch --flake .#$(hostname)
+    end
+    popd
+  '';
+
 }
