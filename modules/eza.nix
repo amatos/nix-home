e75009095a8c88c7ee8919f1b7c7909f298e3851
Alberth Matos <alberth@matos.cc>
2026-08-30 14:52:59 -0400

    feat: add shell aliases for eza
    Signed-off-by: Alberth Matos <alberth@matos.cc>

--- modules/eza.nix
+++ modules/eza.nix
@@ -1,3 +1,19 @@
-{ ... }: {
+{ ... }:
+let
+  ezaAliases = {
+    l = "eza";
+    ll = "eza -l";
+    la = "eza -la";
+    lt = "eza -lT";
+    ltt = "eza -lT --level=2";
+    tree = "eza -T --all --level=3";
+    ls = "eza -abghlUm";
+    big = "eza -lSh";
+  };
+in
+{
   programs.eza.enable = true;
+  programs.zsh.shellAliases = ezaAliases;
+  programs.bash.shellAliases = ezaAliases;
+  programs.fish.shellAliases = ezaAliases;
 }
