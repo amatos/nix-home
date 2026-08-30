96fe277a218fd02ed0a3854c2ec02b8d87489b23
Alberth Matos <alberth@matos.cc>
2026-08-30 14:42:13 -0400

    feat: update starship settings
    Signed-off-by: Alberth Matos <alberth@matos.cc>

--- modules/starship/starship.nix
+++ modules/starship/starship.nix
@@ -1,8 +1,13 @@
-{ ... }: {
+{ ... }:
+let
+  baseSettings = builtins.fromTOML (builtins.readFile ./starship.toml);
+in
+{
   programs.starship = {
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
     enableFishIntegration = true;
+    settings = baseSettings;
   };
 }
